535d61271097feac53f1e41cb64b24b675e362d9
taishikato <taishi.k0903@gmail.com>
2019-08-04 23:35:05 -0700

    :sparkles: Add firebase

---
+++ plugins/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyDYEn7zIIDX9jqXDrToNwBPsJ0g0Hxn7tw',
+    authDomain: 'ask-makers.firebaseapp.com',
+    databaseURL: 'https://ask-makers.firebaseio.com',
+    projectId: 'ask-makers',
+    storageBucket: '',
+    messagingSenderId: '811327399022',
+    appId: '1:811327399022:web:60a82580178913a0'
+  })
+}
+export default firebase
