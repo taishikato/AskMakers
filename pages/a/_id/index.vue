d2eee54c8d471eb36028a6ef3acd6f75b960fb90
taishikato <taishi.k0903@gmail.com>
2019-08-15 10:53:06 -0700

    :sparkles: Make answer page work

--- pages/a/_id/index.vue
+++ pages/a/_id/index.vue
@@ -2,13 +2,42 @@
   <div id="users-id" class="section column is-9 container">
     <div class="columns">
       <div class="column is-8 container">
+        <p
+          v-show="showThankyouBox"
+          class="title has-text-centered weight-800 sp-font"
+        >
+          Thank you for answering!
+        </p>
         <div id="question-img" class="bg-white radius-box">
-          <p id="answer-text">hfjdhjdshfjkdshjkfdsjkfhdjsk</p>
+          <p id="answer-text">{{ answer.content }}</p>
           <img src="/question-ex.jpg" />
-          <div>
-            <p class="title is-3 weight-800 has-text-centered">
-              Share The Answer 👍
+          <div class="is-divider"></div>
+          <div id="share-wrappper">
+            <p class="title is-4 weight-800 has-text-centered">
+              Share the answer
             </p>
+            <ul class="flex-container flex-center">
+              <li>
+                <a href="" class="button facebook is-rounded weight-700">
+                  <span class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                  </span>
+                  <span>
+                    Facebbok
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="" class="button is-rounded is-white weight-700">
+                  <span class="icon">
+                    <i class="fas fa-copy"></i>
+                  </span>
+                  <span>
+                    Copy Sharing URL
+                  </span>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -17,8 +46,52 @@
 </template>
 
 <script>
+import firebase from '~/plugins/firebase'
+// Use firestore
+import 'firebase/firestore'
+const firestore = firebase.firestore()
+
 export default {
-  name: 'AId'
+  name: 'AId',
+  data() {
+    return {
+      answer: {},
+      aId: '',
+      showThankyouBox: false
+    }
+  },
+  validate({ params }) {
+    if (params.id === undefined) {
+      return false
+    }
+    return true
+  },
+  async created() {
+    this.aId = this.$route.params.id
+    const answerData = await firestore
+      .collection('answers')
+      .doc(this.aId)
+      .get()
+    this.answer = answerData.data()
+  },
+  mounted() {
+    const answeredParam = this.getParam('answered')
+    console.log(answeredParam)
+    if (answeredParam === 'true') {
+      this.showThankyouBox = true
+    }
+  },
+  methods: {
+    getParam(name, url) {
+      if (!url) url = window.location.href
+      name = name.replace(/[[\]]/g, '\\$&')
+      const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`)
+      const results = regex.exec(url)
+      if (!results) return null
+      if (!results[2]) return ''
+      return decodeURIComponent(results[2].replace(/\+/g, ' '))
+    }
+  }
 }
 </script>
 
@@ -29,5 +102,13 @@ export default {
 
 #question-img {
   padding: 10px;
+}
+
+.facebook {
+  margin-right: 20px;
+}
+
+#share-wrappper {
+  margin-bottom: 20px;
 }
 </style>
