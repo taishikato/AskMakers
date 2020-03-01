3af1f2fe351661658fcef85e0674d1f6e3d7d205
taishikato <taishi.k0903@gmail.com>
2020-02-29 16:04:27 -0800

    :zap: Use dotenv

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: 'AIzaSyDYEn7zIIDX9jqXDrToNwBPsJ0g0Hxn7tw',
-    authDomain: 'ask-makers.firebaseapp.com',
-    databaseURL: 'https://ask-makers.firebaseio.com',
-    projectId: 'ask-makers',
-    storageBucket: 'ask-makers.appspot.com',
-    messagingSenderId: '811327399022',
-    appId: '1:811327399022:web:60a82580178913a0'
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   })
 }
 export default firebase
