70b9d97658de2dc0eefa0c519e64f4ffc13c8419
taishikato <taishi.k0903@gmail.com>
2020-06-22 16:55:49 -0700

    :zap: Add twitter link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,55 @@
-import React from 'react'
-import Link from 'next/link'
-import { NextPage } from 'next'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import FeaturedMaker from '../components/FeaturedMaker'
-import WelcomeBox from '../components/WelcomeBox'
-import QuestionWrapper from '../components/QuestionWrapper'
-import RecentAnswer from '../components/RecentAnswer'
-import asyncForEach from '../plugins/asyncForEach'
-import firebase from '../plugins/firebase'
-import 'firebase/firestore'
+import React from 'react';
+import Link from 'next/link';
+import { NextPage } from 'next';
+import Layout from '../components/Layout';
+import Hero from '../components/Hero';
+import FeaturedMaker from '../components/FeaturedMaker';
+import WelcomeBox from '../components/WelcomeBox';
+import QuestionWrapper from '../components/QuestionWrapper';
+import RecentAnswer from '../components/RecentAnswer';
+import asyncForEach from '../plugins/asyncForEach';
+import firebase from '../plugins/firebase';
+import 'firebase/firestore';
 
-const db = firebase.firestore()
+const db = firebase.firestore();
 
-const Home: NextPage<Props> = props => {
-  const { questions } = props
-  const [quesionsContainer, setQuesionsContainer] = React.useState(questions)
-  const [lastQuestion, setLastQuestion] = React.useState<ISingleQuestion>()
+const Home: NextPage<Props> = (props) => {
+  const { questions } = props;
+  const [quesionsContainer, setQuesionsContainer] = React.useState(questions);
+  const [lastQuestion, setLastQuestion] = React.useState<ISingleQuestion>();
 
   React.useEffect(() => {
-    setLastQuestion(quesionsContainer[quesionsContainer.length - 1].question)
-  }, [quesionsContainer])
+    setLastQuestion(quesionsContainer[quesionsContainer.length - 1].question);
+  }, [quesionsContainer]);
 
-  const loadQuestions = async e => {
-    e.preventDefault()
-    const quesionsContainerCopy = quesionsContainer.concat()
-    console.log({quesionsContainerCopy})
+  const loadQuestions = async (e) => {
+    e.preventDefault();
+    const quesionsContainerCopy = quesionsContainer.concat();
+    console.log({ quesionsContainerCopy });
     const questionData = await db
       .collection('questions')
       .where('isGeneral', '==', true)
       .orderBy('created', 'desc')
       .startAfter(lastQuestion.created)
       .limit(10)
-      .get()
-    await asyncForEach(questionData.docs, async doc => {
-      const question = doc.data()
+      .get();
+    await asyncForEach(questionData.docs, async (doc) => {
+      const question = doc.data();
       const [userData, answerData, upvoteData] = await Promise.all([
-        db
-          .collection('publicUsers')
-          .doc(question.fromUserId)
-          .get(),
-        db
-          .collection('answers')
-          .where('questionId', '==', question.id)
-          .get(),
+        db.collection('publicUsers').doc(question.fromUserId).get(),
+        db.collection('answers').where('questionId', '==', question.id).get(),
         db
           .collection('questionUpvotes')
           .where('questionId', '==', question.id)
-          .get()
-      ])
-      const user = userData.data()
-      question.answerCount = answerData.size
-      question.questionUpvoteCount = upvoteData.size
-      quesionsContainerCopy.push({ question, user })
-    })
-    setQuesionsContainer(quesionsContainerCopy)
-  }
+          .get(),
+      ]);
+      const user = userData.data();
+      question.answerCount = answerData.size;
+      question.questionUpvoteCount = upvoteData.size;
+      quesionsContainerCopy.push({ question, user });
+    });
+    setQuesionsContainer(quesionsContainerCopy);
+  };
 
   return (
     <Layout>
@@ -75,24 +69,24 @@ const Home: NextPage<Props> = props => {
             <div className="text-xs text-gray-600">
               <div className="mb-3">
                 <Link href="/">
-                  <a>
-                    AskMakers
-                  </a>
+                  <a>AskMakers</a>
                 </Link>
-                , made by Taishi Kato Ⓒ 2020
+                , made by{' '}
+                <a href="https://twitter.com/askmakers_app" target="_blank">
+                  Taishi Kato
+                </a>{' '}
+                Ⓒ 2020
               </div>
               <Link href="/terms-privacy">
-                <a>
-                  Terms of Service & Privacy
-                </a>
+                <a>Terms of Service & Privacy</a>
               </Link>
             </div>
           </aside>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 Home.getInitialProps = async () => {
   const questionData = await db
@@ -100,48 +94,42 @@ Home.getInitialProps = async () => {
     .where('isGeneral', '==', true)
     .orderBy('created', 'desc')
     .limit(10)
-    .get()
-  const questions: any = []
-  await asyncForEach(questionData.docs, async doc => {
-    const question = doc.data()
+    .get();
+  const questions: any = [];
+  await asyncForEach(questionData.docs, async (doc) => {
+    const question = doc.data();
     const [userData, answerData, upvoteData] = await Promise.all([
-      db
-        .collection('publicUsers')
-        .doc(question.fromUserId)
-        .get(),
-      db
-        .collection('answers')
-        .where('questionId', '==', question.id)
-        .get(),
+      db.collection('publicUsers').doc(question.fromUserId).get(),
+      db.collection('answers').where('questionId', '==', question.id).get(),
       db
         .collection('questionUpvotes')
         .where('questionId', '==', question.id)
-        .get()
-    ])
-    const user = userData.data()
-    question.answerCount = answerData.size
-    question.questionUpvoteCount = upvoteData.size
-    questions.push({ question, user })
-  })
-  return { questions }
-}
+        .get(),
+    ]);
+    const user = userData.data();
+    question.answerCount = answerData.size;
+    question.questionUpvoteCount = upvoteData.size;
+    questions.push({ question, user });
+  });
+  return { questions };
+};
 
 interface Props {
-  questions: any
+  questions: any;
 }
 
 interface ISingleQuestion {
-  created: number
-  fromUserId: string
-  id: string
-  image: string
-  isAnswered: boolean
-  isGeneral: boolean
-  slug: string
-  text: string
-  topics: {}
-  answerCount: number
-  questionUpvoteCount: number
+  created: number;
+  fromUserId: string;
+  id: string;
+  image: string;
+  isAnswered: boolean;
+  isGeneral: boolean;
+  slug: string;
+  text: string;
+  topics: {};
+  answerCount: number;
+  questionUpvoteCount: number;
 }
 
-export default Home
+export default Home;
