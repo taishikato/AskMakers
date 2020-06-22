9bc7ca94cb0bdf8dd71562e845d680c6964ce622
taishikato <taishi.k0903@gmail.com>
2020-06-22 16:48:24 -0700

    :zap: use twitter instead of email

--- components/WelcomeBox.tsx
+++ components/WelcomeBox.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { NextPage } from 'next'
+import React from 'react';
+import { NextPage } from 'next';
 
-const WelcomeBox: NextPage<Props> = props => {
+const WelcomeBox: NextPage<Props> = (props) => {
   return (
     <div className={props.class}>
       <p className="font-bold">Welcome, new makers :)</p>
+      <p>I have been working on AskMakers 2.0 for a couple of weeks.</p>
       <p>
-        I have been working on AskMakers 2.0 for a couple of weeks.
-      </p>
-      <p>
-        We welcome any and all feedback. Shoot me a note at taishi.k0903@gmail.com 👍
+        We welcome any and all feedback. Shoot me a note at{' '}
+        <a
+          href="https://twitter.com/askmakers_app"
+          target="_blank"
+          className="text-blue-500"
+        >
+          @askmakers_app
+        </a>{' '}
+        👍
       </p>
     </div>
-  )
-}
+  );
+};
 
 interface Props {
-  class: any
+  class: any;
 }
 
-export default WelcomeBox
+export default WelcomeBox;
