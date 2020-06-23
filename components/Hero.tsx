49d1991eb75531b084ed660763ad8153abcb3482
taishikato <taishi.k0903@gmail.com>
2020-06-22 23:53:21 -0700

    :art: wider hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { NextPage } from 'next'
-import { useSelector } from 'react-redux'
-import AskNowButton from './AskNowButton'
+import React from 'react';
+import { NextPage } from 'next';
+import { useSelector } from 'react-redux';
+import AskNowButton from './AskNowButton';
 
 const Hero: NextPage = () => {
-  const isLogin = useSelector(state => state.isLogin)
+  const isLogin = useSelector((state) => state.isLogin);
   return (
-    <div id="hero-container" className="py-12 h-auto bg-cover text-white px-3 md:px-0 lg:px-0">
-      <div className="mx-auto flex flex-wrap flex-col md:flex-row items-center z-10 h-full w-full md:w-9/12 lg:w-9/12">
+    <div
+      id="hero-container"
+      className="py-12 h-auto bg-cover text-white px-3 md:px-0 lg:px-0"
+    >
+      <div className="mx-auto flex flex-wrap flex-col md:flex-row items-center z-10 h-full w-full md:w-11/12 lg:w-11/12">
         <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left lg:text-left">
-          <h1 className="my-4 text-3xl font-bold leading-tight">Ask experienced makers questions.</h1>
-          <h2 className="leading-normal text-xl mb-8">The best place to ask experienced and successful makers questions.</h2>
-          {!isLogin &&
-          <div className="m-auto md:m-0 lg:m-0">
-            <AskNowButton />
-          </div>
-          }
+          <h1 className="my-4 text-3xl font-bold leading-tight">
+            Ask experienced makers questions.
+          </h1>
+          <h2 className="leading-normal text-xl mb-8">
+            The best place to ask experienced and successful makers questions.
+          </h2>
+          {!isLogin && (
+            <div className="m-auto md:m-0 lg:m-0">
+              <AskNowButton />
+            </div>
+          )}
         </div>
       </div>
       <style jsx>{`
@@ -24,7 +31,7 @@ const Hero: NextPage = () => {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
