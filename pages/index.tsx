ea8ec455e3ede825d2ab3688ac413e80ce64de5a
taishikato <taishi.k0903@gmail.com>
2020-06-23 17:54:34 -0700

    :art: Fix layout for smartphone

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,14 +57,17 @@ const Home: NextPage<Props> = (props) => {
     <Layout>
       {!isLogin && <Hero />}
       <div className="w-full md:w-10/12 lg:w-10/12 mt-5 mb-10 m-auto">
-        <div className="w-full flex flex-wrap -mx-4">
-          <div className="w-full mb-5 md:w-8/12 lg:w-8/12 px-4">
+        <div className="w-full flex flex-wrap px-2 md:-mx-4 lg:-mx-4">
+          <div className="w-full mb-5 md:w-8/12 lg:w-8/12 md:px-4 lg:px-4">
             {quesionsContainer.map((question, index) => (
               <QuestionWrapper question={question} key={index} />
             ))}
             {/* <button onClick={loadQuestions}>Load more</button> */}
           </div>
-          <aside className="w-full md:w-4/12 lg:w-4/12 px-4">
+          <aside
+            className="w-full md:w-4/12 lg:w-4/12 md:px-4 lg:px-4
+          "
+          >
             <WelcomeBox />
             <FeaturedMaker />
             <RecentAnswer />
