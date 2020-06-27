f60a15358d8f344c0a5115bdad622c9d8f48375a
taishikato <taishi.k0903@gmail.com>
2020-06-26 23:06:07 -0700

    :recycle: Delete serverless function and fetch data on browser

--- pages/edit-question/[slug].tsx
+++ pages/edit-question/[slug].tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
+import { useSelector } from 'react-redux';
 import Link from 'next/link';
 import Error from 'next/error';
 import Layout from '../../components/Layout';
@@ -11,6 +12,7 @@ import ReactMde from 'react-mde';
 import MarkdownIt from 'markdown-it';
 import { Checkbox, message } from 'antd';
 import topicOptions from '../../consts/topicOptions';
+import IQuestion from '../../interfaces/IQuestion';
 import firebase from '../../plugins/firebase';
 import 'firebase/firestore';
 
@@ -18,28 +20,48 @@ const db = firebase.firestore();
 
 const mdParser = new MarkdownIt();
 
-const EditQuestionSlug: NextPage<Props> = ({ question, e }) => {
-  if (e === 'not allowed') {
-    return <Error statusCode={404} />;
-  }
+const EditQuestionSlug: NextPage = () => {
+  const router = useRouter();
+  const loginUser = useSelector((state) => state.loginUser);
 
-  const [title, setTitle] = useState(question.text);
-  const [body, setBody] = useState(question.body);
+  const [question, setQuestion] = useState<IQuestion>({} as IQuestion);
+  const [noAuth, setNoAuth] = useState(false);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
   const [topic, setTopic] = useState([]);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [selectedTab, setSelectedTab] = useState<'write' | 'preview'>('write');
 
   useEffect(() => {
+    const getQuestion = async () => {
+      const slug = router.query.slug;
+      const questionData = await db
+        .collection('questions')
+        .where('slug', '==', slug)
+        .get();
+      const question = questionData.docs[0].data();
+      setQuestion(question as IQuestion);
+    };
+    getQuestion();
+  }, []);
+
+  useEffect(() => {
+    if (question.id === undefined) return;
+    if (loginUser.uid !== question.fromUserId) {
+      setNoAuth(true);
+      return;
+    }
+    setTitle(question.text);
+    setBody(question.body);
     if (question.topics.length > 0) {
       setTopic(question.topics);
     }
   }, [question]);
 
-  const ref = db.collection('questions').doc(question.id);
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsSubmitting(true);
+    const ref = db.collection('questions').doc(question.id);
     try {
       await ref.update({
         updated: getUnixTime(),
@@ -47,20 +69,6 @@ const EditQuestionSlug: NextPage<Props> = ({ question, e }) => {
         body,
         topics: topic,
       });
-      // const bodySnapShot = await ref.collection('body').get();
-      // if (bodyState === '') {
-      //   if (!bodySnapShot.empty) {
-      //     const bodyId = bodySnapShot.docs[0].id;
-      //     await ref.collection('body').doc(bodyId).delete();
-      //   }
-      // } else {
-      //   if (bodySnapShot.empty) {
-      //     await ref.collection('body').add({ value: body });
-      //   } else {
-      //     const bodyId = bodySnapShot.docs[0].id;
-      //     await ref.collection('body').doc(bodyId).update({ value: body });
-      //   }
-      // }
       message.success('Submitted successfully');
     } catch (err) {
       message.error('An error occured. Please try again.');
@@ -68,8 +76,6 @@ const EditQuestionSlug: NextPage<Props> = ({ question, e }) => {
       setIsSubmitting(false);
     }
   };
-
-  const router = useRouter();
 
   const pagetitle = `Edit question | AskMakers - Ask experienced makers questions`;
   const url = `https://askmakers.co${router.asPath}`;
@@ -91,101 +97,79 @@ const EditQuestionSlug: NextPage<Props> = ({ question, e }) => {
           content={description}
         />
       </Head>
-      <div className="w-full p-2 md:p-0 lg:p-0 md:w-8/12 lg:w-8/12 m-auto my-10">
-        <h1 className="text-3xl font-medium mb-5">Edit a question</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <Input
-              value={title}
-              id="title"
-              type="text"
-              handleChange={(e) => setTitle(e.target.value)}
-              label="Title"
-              placeholder="How do you validate your idea?"
-              requied={true}
-            />
-          </div>
-          <div className="mb-3">
-            <label className="font-semibold mb-2 block">Body</label>
-            <ReactMde
-              value={body}
-              onChange={setBody}
-              classes={{ textArea: 'focus:outline-none' }}
-              selectedTab={selectedTab}
-              onTabChange={setSelectedTab}
-              generateMarkdownPreview={(markdown) =>
-                Promise.resolve(mdParser.render(markdown))
-              }
-            />
-          </div>
-          <div className="mb-5">
-            <label className="font-semibold mb-2 block">Topic</label>
-            <div className="flex flex-wrap">
-              <Checkbox.Group
-                options={topicOptions}
-                onChange={(checkedValues) => setTopic(checkedValues)}
-                value={topic}
+      {noAuth ? (
+        <Error statusCode={404} />
+      ) : (
+        <div className="w-full p-2 md:p-0 lg:p-0 md:w-8/12 lg:w-8/12 m-auto my-10">
+          <h1 className="text-3xl font-medium mb-5">Edit a question</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <Input
+                value={title}
+                id="title"
+                type="text"
+                handleChange={(e) => setTitle(e.target.value)}
+                label="Title"
+                placeholder="How do you validate your idea?"
+                requied={true}
               />
             </div>
-          </div>
-          <div className="flex flex-wrap">
-            {!isSubmitting && title === '' && (
-              <button className="w-full md:w-auto lg:w-auto px-6 py-3 bg-green-300 rounded text-white font-semibold cursor-not-allowed focus:outline-none">
-                Update your question
-              </button>
-            )}
-            {!isSubmitting && title !== '' && (
-              <button
-                className="w-full md:w-auto lg:w-auto px-6 py-3 bg-green-400 rounded text-white font-semibold hover:bg-green-500 focus:outline-none"
-                type="submit"
-              >
-                Update your question
-              </button>
-            )}
-            {isSubmitting && (
-              <button
-                disabled
-                className="w-full md:w-auto lg:w-auto px-6 py-3 bg-green-300 rounded text-white font-semibold focus:outline-none"
-              >
-                Submitting…
-              </button>
-            )}
-            <Link href="/questions/[slug]" as={`/questions/${question.slug}`}>
-              <a className="w-full md:w-auto lg:w-auto block text-center md:text-left lg:text-left px-6 py-3 bg-white rounded font-semibold hover:underline">
-                Go back to the question page
-              </a>
-            </Link>
-          </div>
-        </form>
-      </div>
+            <div className="mb-3">
+              <label className="font-semibold mb-2 block">Body</label>
+              <ReactMde
+                value={body}
+                onChange={setBody}
+                classes={{ textArea: 'focus:outline-none' }}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                  Promise.resolve(mdParser.render(markdown))
+                }
+              />
+            </div>
+            <div className="mb-5">
+              <label className="font-semibold mb-2 block">Topic</label>
+              <div className="flex flex-wrap">
+                <Checkbox.Group
+                  options={topicOptions}
+                  onChange={(checkedValues) => setTopic(checkedValues)}
+                  value={topic}
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              {!isSubmitting && title === '' && (
+                <button className="w-full md:w-auto lg:w-auto px-6 py-3 bg-green-300 rounded text-white font-semibold cursor-not-allowed focus:outline-none">
+                  Update your question
+                </button>
+              )}
+              {!isSubmitting && title !== '' && (
+                <button
+                  className="w-full md:w-auto lg:w-auto px-6 py-3 bg-green-400 rounded text-white font-semibold hover:bg-green-500 focus:outline-none"
+                  type="submit"
+                >
+                  Update your question
+                </button>
+              )}
+              {isSubmitting && (
+                <button
+                  disabled
+                  className="w-full md:w-auto lg:w-auto px-6 py-3 bg-green-300 rounded text-white font-semibold focus:outline-none"
+                >
+                  Submitting…
+                </button>
+              )}
+              <Link href="/questions/[slug]" as={`/questions/${question.slug}`}>
+                <a className="w-full md:w-auto lg:w-auto block text-center md:text-left lg:text-left px-6 py-3 bg-white rounded font-semibold hover:underline">
+                  Go back to the question page
+                </a>
+              </Link>
+            </div>
+          </form>
+        </div>
+      )}
     </Layout>
   );
 };
-
-EditQuestionSlug.getInitialProps = async (ctx: any) => {
-  const slug = ctx.query.slug;
-  const questionData = await db
-    .collection('questions')
-    .where('slug', '==', slug)
-    .get();
-  const question = questionData.docs[0].data();
-
-  // const bodyData = await db
-  //   .collection('questions')
-  //   .doc(question.id)
-  //   .collection('body')
-  //   .get();
-
-  const loginUser = ctx.store.getState().loginUser;
-  if (loginUser.uid !== question.fromUserId)
-    return { question: {}, e: 'not allowed' };
-
-  return { question };
-};
-
-interface Props {
-  question: any;
-  e?: string;
-}
 
 export default EditQuestionSlug;
