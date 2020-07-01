116dc3c11039cb4a45240db72ee6851847eb1cf9
taishikato <taishi.k0903@gmail.com>
2020-07-01 16:29:39 -0700

    :heavy_minus_sign: delete .tsconfig.json

--- functions/lib/asyncForEach.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = async (array, callback) => {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
-};
-//# sourceMappingURL=asyncForEach.js.map
