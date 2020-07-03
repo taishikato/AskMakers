9288fdfc810b473e89580a2ed8601bb8affb3740
taishikato <taishi.k0903@gmail.com>
2020-07-02 23:29:25 -0700

    :heavy_plus_sign: Add NotFound component

---
+++ components/Common/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center mt-8">
+      <img src="/not-found.svg" width="500" alt="404 page not found" />
+    </div>
+  );
+};
+
+export default NotFound;
