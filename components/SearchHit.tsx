204ca66b101e747b751b7a6e1f7fd80c18eb7c73
taishikato <taishi.k0903@gmail.com>
2020-07-16 12:57:06 -0700

    :art: change the design of search form a bit

--- components/SearchHit.tsx
+++ components/SearchHit.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-const SearchHit = props => {
+const SearchHit = (props) => {
   return (
-    <div className="p-2 text-gray-800 hover:bg-gray-100">
+    <div className="p-2 border-b hover:bg-gray-100">
       <Link href="/questions/[slug]" as={`/questions/${props.hit.slug}`}>
-        <a className="block flex flex-wrap items-center">
+        <a className="block">
           {/* <div className="w-3/12">
             <div className="search-img">
               <img src={props.hit.picture} alt={props.hit.picture} />
             </div>
           </div> */}
           <div className="w-full text-base">
-            <div className="font-bold">{props.hit.text}</div>
-            <div>{props.hit.body}</div>
+            <div className="font-bold text-gray-800">{props.hit.text}</div>
+            <div className="text-gray-700 text-sm">{props.hit.body}</div>
           </div>
         </a>
       </Link>
@@ -27,7 +27,7 @@ const SearchHit = props => {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default SearchHit
+export default SearchHit;
